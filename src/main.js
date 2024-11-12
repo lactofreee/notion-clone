96c8b8abdae7b120ea04a1d870008baca3fd8df8
최민혁 <130155327+lactofreee@users.noreply.github.com>
2024-11-13 00:40:17 +0900

    :recycle: refactor/SPA-router 코드 모듈화 및 분할관리

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createDocumentsList } from "./components/DocumentManager.js";
+import { router } from "./router/router.js";
+
+// 문서 목록 불러오기 및 초기화
+document.addEventListener("DOMContentLoaded", () => {
+  
+  // 초기 라우팅 설정
+  router();
+
+  // 문서 목록 생성
+  createDocumentsList();
+});
