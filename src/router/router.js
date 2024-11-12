4cc66f1ed7fb0abbc114cfc1d68d97dc0c246b65
최민혁 <130155327+lactofreee@users.noreply.github.com>
2024-11-12 23:46:21 +0900

    :bug: fix: 불필요 파일 정리

--- src/router/router.js
+++ src/router/router.js
@@ -1,3 +1,6 @@
+import { test } from "../components/CreateNewDocument";
+
+// API GET 요청으로 root documents tree JSON형태로 가져오기
 const fetchDocumentsInfo = async () => {
   const data = await fetch("https://kdt-api.fe.dev-cos.com/documents", {
     headers: {
@@ -7,22 +10,11 @@ const fetchDocumentsInfo = async () => {
   return await data.json();
 };
 
-// const createDocumentsArray = async () => {
-//   const documents = await fetchDocumentsInfo();
-
-//   const docsTitles = documents.map((doc) => doc.title);
-//   console.log(docsTitles);
-// };
-// createDocumentsArray();
-
-
-
-
-
-
+// 라우트 객체 초기화
 const routes = new Map();
 routes.set("/", "<h1>Home</h1><p>Welcome to the Home Page</p>");
 
+// 라우터 생성
 const router = () => {
   const path = window.location.pathname;
   const content =
@@ -30,17 +22,17 @@ const router = () => {
   document.getElementById("editor__body").innerHTML = content;
 };
 
+// history API 사용하여 SPA 구현
 const navigate = (path) => {
   path = decodeURIComponent(path);
   window.history.pushState({}, path, window.location.origin + path);
   router();
 };
 
-
-
 window.addEventListener("popstate", router);
 document.addEventListener("DOMContentLoaded", router);
 
+// Sidebar에 documents list 목록으로 동적 생성과 라우팅
 const createDocument = (title, content) => {
   const encodedTitle = encodeURIComponent(title);
   const path = `/documents/${encodedTitle}`;
@@ -57,10 +49,8 @@ const createDocument = (title, content) => {
   };
   nav.appendChild(link);
 
-  // 새로 생성한 문서로 이동
   navigate(path);
 };
-
 
 // GET 요청으로 받은 문서 정보를 바탕으로 문서 목록 만들기
 const createDocumentsList = async () => {
@@ -72,3 +62,4 @@ const createDocumentsList = async () => {
 };
 createDocumentsList();
 
+test()
