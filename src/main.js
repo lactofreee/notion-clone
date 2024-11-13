c06f19530474c3026daa001ce94e2ad5f1b10b9d
kangsuyeong <ekdh16@naver.com>
2024-11-13 15:20:36 +0900

    ✨: featt : sidebar show구현

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-// import { createDocumentsList } from "./components/DocumentManager.js";
-// import { router } from "./router/router.js";
+import { createDocumentsList } from "./components/DocumentManager.js";
+import { router } from "./router/router.js";
 
-// // 문서 목록 불러오기 및 초기화
-// document.addEventListener("DOMContentLoaded", () => {
-//   // 초기 라우팅 설정
-//   router();
+// 문서 목록 불러오기 및 초기화
+document.addEventListener("DOMContentLoaded", () => {
+  // 초기 라우팅 설정
+  router();
 
-//   // 문서 목록 생성
-//   createDocumentsList();
-// });
+  // 문서 목록 생성
+  createDocumentsList();
+});
